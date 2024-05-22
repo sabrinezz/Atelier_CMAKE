79d1ca908538b321d03476dab049138652bae000
sabrinezz <165166750+sabrinezz@users.noreply.github.com>
2024-05-22 14:34:10 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,10 +14,7 @@ int main(int argc, char *argv[]){
 		r = _add(atof(a), atof(b));
 		printf("%lf",r);
 	}
-	else if (strcmp(op, "car") == 0) {
-		r = _sub(atof(a));
-		printf("%lf",r);
-	}
+
 	else if (strcmp(op, "sub") == 0) {
 		r = _sub(atof(a), atof(b));
 		printf("%lf",r);
@@ -28,6 +25,10 @@ int main(int argc, char *argv[]){
 	}
 	else if (strcmp(op, "div") == 0) {
 		r = _div(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "car") == 0) {
+		r = _car(atof(a));
 		printf("%lf",r);
 	}
 	else {printf("Erreur de parametres");}
